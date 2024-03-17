922af8c51d3127a02bd34aea8bb1d610647801a1
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-03-17 19:46:39 +0300

    H_W 3(add  data to '.sql' files)

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,15 +2,33 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT c.company_name AS customer, CONCAT(e.first_name, ' ', e.last_name) AS employee
+FROM orders AS o
+JOIN customers AS c USING(customer_id)
+JOIN employees AS e USING(employee_id)
+JOIN shippers AS s ON o.ship_via = s.shipper_id
+WHERE c.city = 'London' AND e.city = 'London' AND o.ship_via = 2
+ORDER BY customer DESC
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
+SELECT product_name, units_in_stock, s.contact_name, s.phone
+FROM suppliers AS s
+JOIN products AS p USING(supplier_id)
+WHERE p.discontinued = 0 AND p.units_in_stock < 25 AND p.category_id IN (2, 4)
+ORDER BY p.units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT company_name FROM customers AS c
+WHERE NOT EXISTS (SELECT customer_id FROM orders AS o WHERE c.customer_id = o.customer_id)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT DISTINCT product_name
+FROM products AS p
+WHERE EXISTS (SELECT quantity FROM order_details WHERE quantity = 10 AND product_id = p.product_id)
